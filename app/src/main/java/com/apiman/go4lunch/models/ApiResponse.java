c0ab6e397b843e42601ea791fc2aa62d39b79c18
willf80 <willyfkouadio@gmail.com>
2019-09-23 13:13:36 +0200

    getRestaurants function added to return the list of restaurants in the result

--- app/src/main/java/com/apiman/go4lunch/models/ApiResponse.java
+++ app/src/main/java/com/apiman/go4lunch/models/ApiResponse.java
@@ -2,13 +2,14 @@ package com.apiman.go4lunch.models;
 
 import com.google.gson.annotations.SerializedName;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ApiResponse {
     private String status;
 
     @SerializedName("results")
-    private List<ApiResults> apiResults;
+    private List<ApiResult> apiResults;
 
     public String getStatus() {
         return status;
@@ -18,12 +19,22 @@ public class ApiResponse {
         this.status = status;
     }
 
-    public List<ApiResults> getApiResults() {
+    public List<ApiResult> getApiResults() {
         return apiResults;
     }
 
-    public void setApiResults(List<ApiResults> apiResults) {
+    public void setApiResults(List<ApiResult> apiResults) {
         this.apiResults = apiResults;
+    }
+
+    public List<Restaurant> getRestaurants() {
+        List<Restaurant> restaurants = new ArrayList<>();
+        for (ApiResult results :
+                apiResults) {
+            restaurants.add(results.toRestaurant());
+        }
+
+        return restaurants;
     }
 }
 
